b53e790a7bb91ebdcfc94d474fd5c0f0a57acdc4
Murtaza824 <66223556+Murtaza824@users.noreply.github.com>
2022-12-01 13:47:04 -0500

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -7,7 +7,7 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const basePromptPrefix = `
-Take the content below and generate a twitter thread written in the style of Paul Graham. Make it feel like a story. Don't just list the points. Go deep into each one. Explain why.
+Take the content below and turn it into a twitter thread written in the style of Paul Graham. Make it feel like a story. Don't just list the points. Go deep into each one. Explain why.
 
 Content:
 `;
