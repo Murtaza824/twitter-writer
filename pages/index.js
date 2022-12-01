4a647dd4fc70acbe48c7f14de7a88194aa497d54
Murtaza824 <66223556+Murtaza824@users.noreply.github.com>
2022-12-01 13:18:50 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -33,26 +33,6 @@ const callGenerateEndpoint = async () => {
     setUserInput(event.target.value);
   };
 
-  const callGenerateEndpoint = async () => {
-    setIsGenerating(true);
-
-    console.log("Calling OpenAI...");
-    const response = await fetch("/api/generate", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ userInput }),
-    });
-
-    const data = await response.json();
-    const { output } = data;
-    console.log("OpenAI replied...", output.text);
-
-    setApiOutput(`${output.text}`);
-    setIsGenerating(false);
-  };
-
   return (
     <div className="root">
       <div className="container">
