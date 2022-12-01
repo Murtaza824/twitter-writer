a79a314c9ab0f11e389b5dbcaf7662a2f75cd142
Murtaza824 <66223556+Murtaza824@users.noreply.github.com>
2022-12-01 11:13:30 -0500

    Add files via upload

---
+++ pages/api/generate.js
@@ -0,0 +1,30 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix = `
+Take the content below and generate a twitter thread written in the style of Paul Graham. Make it feel like a story. Don't just list the points. Go deep into each one. Explain why.
+
+Content:
+`;
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}\n`);
+
+  const baseCompletion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    temperature: 0.85,
+    max_tokens: 1250,
+  });
+
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
---
+++ pages/index.js
@@ -0,0 +1,91 @@
+import Head from "next/head";
+import Image from "next/image";
+import buildspaceLogo from "../assets/buildspace-logo.png";
+import { useState } from "react";
+
+const Home = () => {
+  const [userInput, setUserInput] = useState("");
+  const onUserChangedText = (event) => {
+    console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
+
+  const [apiOutput, setApiOutput] = useState("");
+  const [isGenerating, setIsGenerating] = useState(false);
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+
+    console.log("Calling OpenAI...");
+    console.log(userInput);
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userInput }),
+    });
+
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text);
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  };
+
+  return (
+    <div className="root">
+      <div className="container">
+        <div className="header">
+          <div className="header-title">
+            <h1>Twitter Thread Generator</h1>
+          </div>
+          <div className="header-subtitle">
+            <h2>
+              Turn any sort of written content into Twitter threads within a few
+              seconds
+            </h2>
+          </div>
+        </div>
+        <div className="prompt-container">
+          <textarea
+            placeholder="Write or paste your contents here :)"
+            className="prompt-box"
+            value={userInput}
+            onChange={onUserChangedText}
+          />
+        </div>
+        <div className="prompt-buttons">
+          <a
+            className={
+              isGenerating ? "generate-button loading" : "generate-button"
+            }
+            onClick={callGenerateEndpoint}
+          >
+            <div className="generate">
+              {isGenerating ? <span class="loader"></span> : <p>Generate</p>}
+            </div>
+          </a>
+        </div>
+      </div>
+      (
+      <div className="output">
+        <div className="output-header-container">
+          <div className="output-header">
+            <h3>Output</h3>
+          </div>
+          <div className="output-content">
+            <p>{apiOutput}</p>
+          </div>
+        </div>
+      </div>
+      ) ;
+    </div>
+
+    // closes return function
+  );
+
+  // closes Home Component
+};
+export default Home;
